e0056117617fe214d7cffdf72bf76983a30ff6da
daikidev111 <dkub0001@student.monash.edu>
2022-02-25 21:18:10 +0900

    Implemented transactions

--- pkg/server/handler/gacha.go
+++ pkg/server/handler/gacha.go
@@ -1,12 +1,14 @@
 package handler
 
 import (
+	"database/sql"
 	"encoding/json"
 	"log"
 	"math/rand"
 	"net/http"
 
 	"22dojo-online/pkg/constant"
+	"22dojo-online/pkg/db"
 	"22dojo-online/pkg/dcontext"
 	"22dojo-online/pkg/http/response"
 	"22dojo-online/pkg/server/model"
@@ -19,7 +21,7 @@ type gachaResponse struct {
 	IsNew        bool   `json:"isNew"`
 }
 
-type gachaListResponse struct {
+type GachaListResponse struct {
 	Result []*gachaResponse `json:"gachaList"`
 }
 
@@ -56,128 +58,133 @@ func HandleGachaPost() http.HandlerFunc {
 			return
 		}
 
-		user, err := model.SelectUserByPrimaryKey(userID)
-		if err != nil {
-			log.Println(err)
-			response.InternalServerError(writer, "Internal Server Error")
-			return
-		}
-
-		times := int32(requestBody.Times)
-		if user.Coin < constant.GachaCoinConsumption*times {
-			log.Println("The amount of coins the user has is less than the consumption of coin for the gacha draw")
-			response.BadRequest(writer, "Invalid gacha draw")
-			return
-		}
+		gachaCollectionList := make([]*gachaResponse, 0, requestBody.Times) // Response用のスライス
 
 		// トランザクションをここから開始する
+		err := db.Transact(ctx, db.Conn, func(tx *sql.Tx) error {
+			user, err := model.SelectUserByPrimaryKeyWithLock(userID, tx)
+			if err != nil {
+				log.Println(err)
+				response.InternalServerError(writer, "Internal Server Error")
+				return err
+			}
 
-		collectionItems, err := model.SelectAllCollectionItems()
-		if err != nil {
-			log.Println(err)
-			response.InternalServerError(writer, "Internal Server Error")
-			return
-		}
-		if len(collectionItems) == 0 {
-			log.Println("A collection of items is not found")
-			response.BadRequest(writer, "A collection of items is not found.")
-			return
-		}
+			times := int32(requestBody.Times)
+			if user.Coin < constant.GachaCoinConsumption*times {
+				log.Println("The amount of coins the user has is less than the consumption of coin for the gacha draw")
+				response.BadRequest(writer, "Invalid gacha draw")
+				return err
+			}
 
-		userCollectionItems, err := model.SelectUserCollectionItemByUserID(userID)
-		if err != nil {
-			log.Println(err)
-			response.InternalServerError(writer, "Internal Server Error")
-			return
-		}
+			collectionItems, err := model.SelectAllCollectionItems()
+			if err != nil {
+				log.Println(err)
+				response.InternalServerError(writer, "Internal Server Error")
+				return err
+			}
+			if len(collectionItems) == 0 {
+				log.Println("A collection of items is not found")
+				response.BadRequest(writer, "A collection of items is not found.")
+				return err
+			}
 
-		gachaProbabilities, err := model.SelectAllCollectionItemProbability()
-		if err != nil {
-			log.Println(err)
-			response.InternalServerError(writer, "Internal Server Error")
-			return
-		}
+			userCollectionItems, err := model.SelectUserCollectionItemByUserIDWithLock(tx, userID)
+			if err != nil {
+				log.Println(err)
+				response.InternalServerError(writer, "Internal Server Error")
+				return err
+			}
 
-		UserCollcetionItemsArr := make([]*model.UserCollectionItem, 0, times)                // times -1かどうかの確認　Gachaで取得された新アイテムの格納
-		gachaCollectionList := make([]*gachaResponse, 0, times)                              // Response用のスライス
-		itemCollectionMap := make(map[string]*model.CollectionItem, len(collectionItems))    // itemCollectionのマップ
-		userCollectionItemsMap := make(map[string]bool, len(userCollectionItems)+int(times)) // user_collection_itemsのマップ
+			gachaProbabilities, err := model.SelectAllCollectionItemProbability()
+			if err != nil {
+				log.Println(err)
+				response.InternalServerError(writer, "Internal Server Error")
+				return err
+			}
 
-		// すでに所持しているかをIDを突き合わせて判定するためのマップ(動的）
-		for _, userCollectionItem := range userCollectionItems {
-			userCollectionItemsMap[userCollectionItem.CollectionID] = true
-		}
+			UserCollcetionItemsArr := make([]*model.UserCollectionItem, 0, times)                // times -1かどうかの確認　Gachaで取得された新アイテムの格納
+			itemCollectionMap := make(map[string]*model.CollectionItem, len(collectionItems))    // itemCollectionのマップ
+			userCollectionItemsMap := make(map[string]bool, len(userCollectionItems)+int(times)) // user_collection_itemsのマップ
 
-		// IDのキーと格アイテムの情報が入っている静的マップの生成
-		for i, collectionItem := range collectionItems {
-			itemCollectionMap[collectionItem.ID] = collectionItems[i]
-		}
+			// すでに所持しているかをIDを突き合わせて判定するためのマップ(動的）
+			for _, userCollectionItem := range userCollectionItems {
+				userCollectionItemsMap[userCollectionItem.CollectionID] = true
+			}
 
-		var SumOfRatio int
-		for _, gachaProb := range gachaProbabilities {
-			SumOfRatio += int(gachaProb.Ratio) //TODO:  cast を治す -> 構造体のratioをイントにしても良いかも？？
-		}
+			// IDのキーと格アイテムの情報が入っている静的マップの生成
+			for i, collectionItem := range collectionItems {
+				itemCollectionMap[collectionItem.ID] = collectionItems[i]
+			}
 
-		// =========== start of the loop ===================
-		for i := 0; i < requestBody.Times; i++ {
-
-			randInt := rand.Intn(SumOfRatio) // 乱数の取得
-
-			var targetRatio int           // Ratioから取得される値を足す際に必要
-			var targetCollectionID string // 乱数を越した際のcollection ID(ガチャの引きアイテム)
-
-			// ガチャで排出確率に基づいたコレクションアイテムの取得
+			var SumOfRatio int
 			for _, gachaProb := range gachaProbabilities {
-				targetRatio += int(gachaProb.Ratio) // TODO: cast直し
-				if targetRatio > randInt {
-					targetCollectionID = gachaProb.CollectionID
-					break
+				SumOfRatio += int(gachaProb.Ratio) //TODO:  cast を治す -> 構造体のratioをイントにしても良いかも？？
+			}
+
+			// =========== start of the loop ===================
+			for i := 0; i < requestBody.Times; i++ {
+
+				randInt := rand.Intn(SumOfRatio) // 乱数の取得
+
+				var targetRatio int           // Ratioから取得される値を足す際に必要
+				var targetCollectionID string // 乱数を越した際のcollection ID(ガチャの引きアイテム)
+
+				// ガチャで排出確率に基づいたコレクションアイテムの取得
+				for _, gachaProb := range gachaProbabilities {
+					targetRatio += int(gachaProb.Ratio) // TODO: cast直し
+					if targetRatio > randInt {
+						targetCollectionID = gachaProb.CollectionID
+						break
+					}
+				}
+
+				// 共通処理: responseに格納
+				gachaCollectionList = append(gachaCollectionList, &gachaResponse{
+					CollectionID: targetCollectionID,
+					Name:         itemCollectionMap[targetCollectionID].Name,
+					Rarity:       itemCollectionMap[targetCollectionID].Rarity,
+					IsNew:        !userCollectionItemsMap[targetCollectionID], // 新しく獲得したアイテムはisNewがtrue,既に持っているアイテムはisNewがfalse
+				})
+
+				// 新アイテムはUserCollcetionItemsArrに格納(bulk insert時に必要となる)
+				if !userCollectionItemsMap[targetCollectionID] {
+					UserCollcetionItemsArr = append(UserCollcetionItemsArr, &model.UserCollectionItem{ // TODO: fix typo UserCollcetionItemsArr
+						UserID:       userID,
+						CollectionID: targetCollectionID,
+					})
+					userCollectionItemsMap[targetCollectionID] = true // 格納後はtrueに変換する
+				}
+			}
+			// ========== end of the loop ==============
+
+			// bulk insertの開始
+			if len(UserCollcetionItemsArr) > 0 {
+				err = model.InsertUserCollectionItemsByUserIDWithLock(tx, UserCollcetionItemsArr)
+				if err != nil {
+					log.Println("Failed to insert the new item into the user's collection", err)
+					response.InternalServerError(writer, "Internal Server Error")
+					return err
 				}
 			}
 
-			// 共通処理: responseに格納
-			gachaCollectionList = append(gachaCollectionList, &gachaResponse{
-				CollectionID: targetCollectionID,
-				Name:         itemCollectionMap[targetCollectionID].Name,
-				Rarity:       itemCollectionMap[targetCollectionID].Rarity,
-				IsNew:        !userCollectionItemsMap[targetCollectionID], // 新しく獲得したアイテムはisNewがtrue,既に持っているアイテムはisNewがfalse
-			})
-
-			// 新アイテムはUserCollcetionItemsArrに格納(bulk insert時に必要となる)
-			if !userCollectionItemsMap[targetCollectionID] {
-				UserCollcetionItemsArr = append(UserCollcetionItemsArr, &model.UserCollectionItem{ // TODO: fix typo UserCollcetionItemsArr
-					UserID:       userID,
-					CollectionID: targetCollectionID,
-				})
-				userCollectionItemsMap[targetCollectionID] = true // 格納後はtrueに変換する
-			}
-		}
-		// ========== end of the loop ==============
-
-		// bulk insertの開始
-		if len(UserCollcetionItemsArr) > 0 {
-			err = model.InsertUserCollectionItemsByUserID(UserCollcetionItemsArr)
+			//コイン消費（コインをマイナスにしてアップデート処理）
+			user.Coin -= constant.GachaCoinConsumption * times
+			err = model.UpdateCoinAndScoreByPrimaryKeyWithLock(tx, userID, user.HighScore, user.Coin)
 			if err != nil {
-				log.Println("Failed to insert the new item into the user's collection", err)
+				log.Println("Failed to update the user's coin", err)
 				response.InternalServerError(writer, "Internal Server Error")
-				return
+				return err
 			}
-		}
 
-		//コイン消費（コインをマイナスにしてアップデート処理）
-		user.Coin -= constant.GachaCoinConsumption * times
-		err = model.UpdateCoinByPrimaryKey(userID, user.Coin)
-		if err != nil {
-			log.Println("Failed to update the user's coin", err)
-			response.InternalServerError(writer, "Internal Server Error")
-			return
-		}
+			return nil
+		})
+		log.Println(err)
 
 		// responseを返す
-		response.Success(writer, &gachaListResponse{
+		response.Success(writer, &GachaListResponse{
 			Result: gachaCollectionList,
 		})
 	}
 }
 
-// transaction -> select user by primary key, select all user collection items, update coin, inseruser collections,
+// transaction -> inseruser collections,
--- pkg/server/model/user_collection_item.go
+++ pkg/server/model/user_collection_item.go
@@ -24,8 +24,17 @@ func SelectUserCollectionItemByUserID(userID string) ([]*UserCollectionItem, err
 	return convertToUserCollectionItems(rows)
 }
 
+func SelectUserCollectionItemByUserIDWithLock(tx *sql.Tx, userID string) ([]*UserCollectionItem, error) {
+	rows, err := tx.Query("SELECT user_id, collection_item_id FROM user_collection_item where user_id = ? FOR UPDATE;", userID)
+	if err != nil {
+		return nil, err
+	}
+
+	return convertToUserCollectionItems(rows)
+}
+
 //TODO: ADD TRANSACTION !!!!!!!
-func InsertUserCollectionItemsByUserID(userCollectionItems []*UserCollectionItem) error {
+func InsertUserCollectionItemsByUserIDWithLock(tx *sql.Tx, userCollectionItems []*UserCollectionItem) error {
 	valueStrings := make([]string, 0, len(userCollectionItems))
 	valueArgs := make([]interface{}, 0, len(userCollectionItems)*3)
 	for _, userCollectionItem := range userCollectionItems {
@@ -35,26 +44,10 @@ func InsertUserCollectionItemsByUserID(userCollectionItems []*UserCollectionItem
 	}
 	stmt := fmt.Sprintf("INSERT INTO user_collection_item (user_id, collection_item_id) VALUES %s",
 		strings.Join(valueStrings, ","))
-	_, err := db.Conn.Exec(stmt, valueArgs...) // call Statusln with a variable number of arguments
+	_, err := tx.Exec(stmt, valueArgs...) // call Statusln with a variable number of arguments
 	// maybe remove statusln and directly access to the index of the arrays
 	return err
-
 }
-
-// func BulkInsert(unsavedRows []*gachaResponse) error {
-// 	valueStrings := make([]string, 0, len(unsavedRows))
-// 	valueArgs := make([]interface{}, 0, len(unsavedRows)*3)
-// 	for _, post := range unsavedRows {
-// 		valueStrings = append(valueStrings, "(?, ?, ?)")
-// 		valueArgs = append(valueArgs, post.Column1)
-// 		valueArgs = append(valueArgs, post.Column2)
-// 		valueArgs = append(valueArgs, post.Column3)
-// 	}
-// 	stmt := fmt.Sprintf("INSERT INTO my_sample_table (column1, column2, column3) VALUES %s",
-// 		strings.Join(valueStrings, ","))
-// 	_, err := db.Exec(stmt, valueArgs...)
-// 	return err
-// }
 
 // convertToUserCollectionItemsでrowデータをUserCollectionItemデータへ変換する
 func convertToUserCollectionItems(rows *sql.Rows) ([]*UserCollectionItem, error) {
