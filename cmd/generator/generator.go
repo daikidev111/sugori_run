b84a2f9206ed1e553fec6eef00b584695743c53f
daikidev111 <dkub0001@student.monash.edu>
2022-03-02 00:36:45 +0900

    Make a consideration for a mix of lower and upper case characters in the model option line

--- cmd/generator/generator.go
+++ cmd/generator/generator.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"html/template"
 	"log"
 	"os"
 	"strings"
+	"unicode"
 )
 
 type Entity struct {
@@ -44,18 +46,33 @@ func main() {
 	// go run generator.go model="User"
 	f := flag.String("model", "None", "This is to generate files based on the model name")
 	flag.Parse()
+	// fmt.Println(*f)
+	words := strings.Fields(*f)
 
-	createEntityFile(*f)
-	createDomainRepositoryFile(*f)
-	createServiceFile(*f)
-	createDatabaseRepositoryFile(*f)
-	createInteractorFile(*f)
-	createControllerFile(*f)
+	// fmt.Println(words)
+
+	var model string
+	var modelFCharLowerCase string
+	if len(words) > 1 {
+		for _, v := range words {
+			model += strings.Title(strings.ToLower(v))
+		}
+		modelFCharLowerCase = FirstCharToLowerCase(model)
+	} else if len(words) == 1 {
+		model = strings.Title(strings.ToLower(*f))
+		modelFCharLowerCase = FirstCharToLowerCase(model)
+	} else {
+		log.Println("Does not accept empty word")
+	}
+	// createEntityFile(model)
+	// createDomainRepositoryFile(model)
+	createServiceFile(model, modelFCharLowerCase)
+	// createDatabaseRepositoryFile(model)
+	// createInteractorFile(model, modelFCharLowerCase)
+	// createControllerFile(model, modelFCharLowerCase)
 }
 
 func createEntityFile(model string) {
-	model = strings.Title(strings.ToLower(model))
-
 	data := Entity{
 		Package: "entity",
 		Model:   model,
@@ -82,8 +99,6 @@ func createEntityFile(model string) {
 }
 
 func createDomainRepositoryFile(model string) {
-	model = strings.Title(strings.ToLower(model))
-
 	data := DomainRepository{
 		Package: "entity",
 		Model:   model,
@@ -108,13 +123,12 @@ func createDomainRepositoryFile(model string) {
 	}
 }
 
-func createServiceFile(model string) {
-	model = strings.Title(strings.ToLower(model))
-	modelLowerCase := strings.ToLower(model)
+func createServiceFile(model string, modelLower string) {
+	fmt.Println(modelLower)
 	data := DomainService{
 		Package:        "service",
 		Model:          model,
-		ModelLowerCase: modelLowerCase,
+		ModelLowerCase: modelLower,
 	}
 	template, err := template.New("domain_service.tmpl").ParseGlob("domain_service.tmpl")
 	if err != nil {
@@ -136,7 +150,6 @@ func createServiceFile(model string) {
 }
 
 func createDatabaseRepositoryFile(model string) {
-	model = strings.Title(strings.ToLower(model))
 	data := DatabaseRepository{
 		Package: "database",
 		Model:   model,
@@ -160,13 +173,11 @@ func createDatabaseRepositoryFile(model string) {
 	}
 }
 
-func createInteractorFile(model string) {
-	model = strings.Title(strings.ToLower(model))
-	modelLowerCase := strings.ToLower(model)
+func createInteractorFile(model string, modelLower string) {
 	data := Interactor{
 		Package:        "interactor",
 		Model:          model,
-		ModelLowerCase: modelLowerCase,
+		ModelLowerCase: modelLower,
 	}
 	template, err := template.New("interactor.tmpl").ParseGlob("interactor.tmpl")
 	if err != nil {
@@ -187,13 +198,11 @@ func createInteractorFile(model string) {
 	}
 }
 
-func createControllerFile(model string) {
-	model = strings.Title(strings.ToLower(model))
-	modelLowerCase := strings.ToLower(model)
+func createControllerFile(model string, modelLower string) {
 	data := Controller{
 		Package:        "controller",
 		Model:          model,
-		ModelLowerCase: modelLowerCase,
+		ModelLowerCase: modelLower,
 	}
 	template, err := template.New("controller.tmpl").ParseGlob("controller.tmpl")
 	if err != nil {
@@ -222,3 +231,11 @@ func createControllerFile(model string) {
 	// ParseFiles -> which template file
 	// New
 */
+
+func FirstCharToLowerCase(model string) string {
+	s := model
+	a := []rune(s)
+	a[0] = unicode.ToLower(a[0])
+	s = string(a)
+	return s
+}
