c90b43acebba1e36ae8492be0f3415bf53eea1d4
daikidev111 <dkub0001@student.monash.edu>
2022-03-01 22:30:40 +0900

    entity file sucessfully generated

--- cmd/generator/generator.go
+++ cmd/generator/generator.go
@@ -2,41 +2,47 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"html/template"
 	"log"
 	"os"
-	"text/template"
+	"strings"
 )
 
-type Language struct {
-	Name string
-	URL  string
+type Entity struct {
+	Package string
+	Model   string
+	Models  string
 }
+
+var FileExtension = ".go"
 
 //  go run generator.go -model="User"
 func main() {
 
-	// load template file
-	// template, err := template.New("index.html.tmpl").ParseFiles("index.html.tmpl")
-	// ParseFiles -> which template file
-	// New
-
 	// go run generator.go model="User"
 	f := flag.String("model", "None", "This is to generate files based on the model name")
 	flag.Parse()
-	fmt.Println(*f)
 
-	data := Language{
-		Name: `Go`,
-		URL:  `https://golang.org/`,
+	createEntityFile(*f)
+}
+
+func createEntityFile(model string) {
+	model = strings.Title(strings.ToLower(model))
+
+	data := Entity{
+		Package: "entity",
+		Model:   model,
+		Models:  model + "s",
 	}
-	template, err := template.New("test.txt").ParseFiles("test.txt")
+	template, err := template.New("entity.tmpl").ParseFiles("entity.tmpl")
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	fileName := strings.ToLower(model) + FileExtension
+
 	// ファイルの生成
-	fp, err := os.Create("index.txt")
+	fp, err := os.Create(fileName)
 	if err != nil {
 		log.Println("error creating file", err)
 	}
@@ -47,3 +53,14 @@ func main() {
 		log.Fatal(err)
 	}
 }
+
+//　関数化する
+// entity の構造体を作る
+// make the first character capitalized
+
+/*
+	// load template file
+	// template, err := template.New("index.html.tmpl").ParseFiles("index.html.tmpl")
+	// ParseFiles -> which template file
+	// New
+*/
