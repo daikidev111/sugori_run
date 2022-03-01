bf6bde51c85acd7a2846e394577332a3f9ee400d
daikidev111 <dkub0001@student.monash.edu>
2022-03-01 23:55:36 +0900

    Implement file generator functions

--- cmd/generator/generator.go
+++ cmd/generator/generator.go
@@ -13,17 +13,44 @@ type Entity struct {
 	Model   string
 	Models  string
 }
+type DomainRepository struct {
+	Package string
+	Model   string
+}
+type DomainService struct {
+	Package        string
+	Model          string
+	ModelLowerCase string
+}
+type DatabaseRepository struct {
+	Package string
+	Model   string
+}
+type Interactor struct {
+	Package        string
+	Model          string
+	ModelLowerCase string
+}
+type Controller struct {
+	Package        string
+	Model          string
+	ModelLowerCase string
+}
 
 var FileExtension = ".go"
 
 //  go run generator.go -model="User"
 func main() {
-
 	// go run generator.go model="User"
 	f := flag.String("model", "None", "This is to generate files based on the model name")
 	flag.Parse()
 
 	createEntityFile(*f)
+	createDomainRepositoryFile(*f)
+	createServiceFile(*f)
+	createDatabaseRepositoryFile(*f)
+	createInteractorFile(*f)
+	createControllerFile(*f)
 }
 
 func createEntityFile(model string) {
@@ -48,15 +75,146 @@ func createEntityFile(model string) {
 	}
 	defer fp.Close()
 
-	// write file
+	// ファイルへの書き込み
 	if err = template.Execute(fp, data); err != nil {
 		log.Fatal(err)
 	}
 }
 
-//　関数化する
-// entity の構造体を作る
-// make the first character capitalized
+func createDomainRepositoryFile(model string) {
+	model = strings.Title(strings.ToLower(model))
+
+	data := DomainRepository{
+		Package: "entity",
+		Model:   model,
+	}
+	template, err := template.New("domain_repo.tmpl").ParseGlob("domain_repo.tmpl")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileName := strings.ToLower(model) + "_repository" + FileExtension
+
+	// ファイルの生成
+	fp, err := os.Create(fileName)
+	if err != nil {
+		log.Println("error creating file", err)
+	}
+	defer fp.Close()
+
+	// ファイルへの書き込み
+	if err = template.Execute(fp, data); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createServiceFile(model string) {
+	model = strings.Title(strings.ToLower(model))
+	modelLowerCase := strings.ToLower(model)
+	data := DomainService{
+		Package:        "service",
+		Model:          model,
+		ModelLowerCase: modelLowerCase,
+	}
+	template, err := template.New("domain_service.tmpl").ParseGlob("domain_service.tmpl")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileName := data.ModelLowerCase + "_service" + FileExtension
+
+	fp, err := os.Create(fileName)
+	if err != nil {
+		log.Println("error creating file", err)
+	}
+	defer fp.Close()
+
+	// ファイルへの書き込み
+	if err = template.Execute(fp, data); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createDatabaseRepositoryFile(model string) {
+	model = strings.Title(strings.ToLower(model))
+	data := DatabaseRepository{
+		Package: "database",
+		Model:   model,
+	}
+	template, err := template.New("database_repository.tmpl").ParseGlob("database_repository.tmpl")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileName := strings.ToLower(model) + "_repository" + FileExtension
+
+	fp, err := os.Create(fileName)
+	if err != nil {
+		log.Println("error creating file", err)
+	}
+	defer fp.Close()
+
+	// ファイルへの書き込み
+	if err = template.Execute(fp, data); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createInteractorFile(model string) {
+	model = strings.Title(strings.ToLower(model))
+	modelLowerCase := strings.ToLower(model)
+	data := Interactor{
+		Package:        "interactor",
+		Model:          model,
+		ModelLowerCase: modelLowerCase,
+	}
+	template, err := template.New("interactor.tmpl").ParseGlob("interactor.tmpl")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileName := data.ModelLowerCase + "_interactor" + FileExtension
+
+	fp, err := os.Create(fileName)
+	if err != nil {
+		log.Println("error creating file", err)
+	}
+	defer fp.Close()
+
+	// ファイルへの書き込み
+	if err = template.Execute(fp, data); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createControllerFile(model string) {
+	model = strings.Title(strings.ToLower(model))
+	modelLowerCase := strings.ToLower(model)
+	data := Controller{
+		Package:        "controller",
+		Model:          model,
+		ModelLowerCase: modelLowerCase,
+	}
+	template, err := template.New("controller.tmpl").ParseGlob("controller.tmpl")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileName := data.ModelLowerCase + "_controller" + FileExtension
+
+	fp, err := os.Create(fileName)
+	if err != nil {
+		log.Println("error creating file", err)
+	}
+	defer fp.Close()
+
+	// ファイルへの書き込み
+	if err = template.Execute(fp, data); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// DI for databse repository
 
 /*
 	// load template file
