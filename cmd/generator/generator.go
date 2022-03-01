c86ced51647e64f49c9a7be3de9a72af7f71e3c6
daikidev111 <dkub0001@student.monash.edu>
2022-03-01 14:02:22 +0900

    Implement file generator

--- cmd/generator/generator.go
+++ cmd/generator/generator.go
@@ -1,17 +1,41 @@
 package main
 
-// import (
-// 	"log"
-// 	"os"
-// 	"text/template"
-// )
+import (
+	"log"
+	"os"
+	"text/template"
+)
 
-// func main() {
-// 	template, err := template.ParseFiles("template-01.txt")
-// 	// Capture any error
-// 	if err != nil {
-// 		log.Fatalln(err)
-// 	}
-// 	// Print out the template to std
-// 	template.Execute(os.Stdout, nil)
-// }
+type Language struct {
+	Name string
+	URL  string
+}
+
+func main() {
+
+	// load template file
+	// template, err := template.New("index.html.tmpl").ParseFiles("index.html.tmpl")
+	// ParseFiles -> which template file
+	// New
+
+	data := Language{
+		Name: `Go`,
+		URL:  `https://golang.org/`,
+	}
+	template, err := template.New("test.txt").ParseFiles("test.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// ファイルの生成
+	fp, err := os.Create("index.txt")
+	if err != nil {
+		log.Println("error creating file", err)
+	}
+	defer fp.Close()
+
+	// write file
+	if err = template.Execute(fp, data); err != nil {
+		log.Fatal(err)
+	}
+}
