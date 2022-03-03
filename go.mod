8d026341f69d62fb843142ac05ba539329db7272
daikidev111 <dkub0001@student.monash.edu>
2022-03-04 07:57:29 +0900

    Mock test for service is added

--- go.mod
+++ go.mod
@@ -12,12 +12,17 @@ require (
 	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/golang/mock v1.6.0 // indirect
 	github.com/google/go-cmp v0.5.7 // indirect
 	github.com/kisielk/sqlstruct v0.0.0-20210630145711-dae28ed37023 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.1.0 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+	golang.org/x/tools v0.1.1 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gopkg.in/go-playground/validator.v9 v9.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
--- pkg/domain/service/mock/user_service_test.go
+++ pkg/domain/service/mock/user_service_test.go
@@ -1,38 +1,24 @@
-package mock
+package mock_service
 
 import (
-	"22dojo-online/pkg/domain/entity"
-	"22dojo-online/pkg/domain/repository"
-	"22dojo-online/pkg/domain/service"
-	"fmt"
+	entity "22dojo-online/pkg/domain/entity"
 	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
 )
-
-type userServiceFaker struct {
-	UserRepository repository.UserRepository
-}
-
-func (usf *userServiceFaker) SelectUserByPrimaryKeyF(userID string) (*entity.User, error) {
-	user, err := usf.UserRepository.SelectUserByPrimaryKey(userID)
-	if err != nil {
-		return nil, fmt.Errorf("failed to query a user by primary key. err = %w", err)
-	}
-	return user, nil
-}
 
 // test
 func TestSelectUserByPrimaryKeyService(t *testing.T) {
 	table := []struct {
-		testName     string
-		id           string
-		nameToChange string
-		user         entity.User
-		err          error
+		testName string
+		id       string
+		user     entity.User
+		err      error
 	}{
 		{
-			"FIRST TEST CASE: InsertUser from pkg/interfaces/database/user_repository.go",
+			"FIRST TEST CASE: TestSelectUserByPrimaryKeyService",
 			"78164dcf-6b7c-45e4-862a-2a0f6735a449",
-			"whattt",
 			entity.User{
 				ID:        "78164dcf-6b7c-45e4-862a-2a0f6735a449",
 				AuthToken: "b187b9e0-08e6-42dd-a9b3-a900b137983c",
@@ -42,19 +28,201 @@ func TestSelectUserByPrimaryKeyService(t *testing.T) {
 			},
 			nil,
 		},
+		{
+			"SECOND TEST CASE: TestSelectUserByPrimaryKeyService",
+			"829bdb53-f322-40bb-9327-63ab00536cd3",
+			entity.User{
+				ID:        "829bdb53-f322-40bb-9327-63ab00536cd3",
+				AuthToken: "4ed9e2e7-cbba-4ab9-a669-14b688dfb245",
+				Name:      "bruh",
+				HighScore: 0,
+				Coin:      0,
+			},
+			nil,
+		},
+		{
+			"THIRD TEST CASE: TestSelectUserByPrimaryKeyService",
+			"909b42f8-cce9-4d02-bce4-e7c7e28df550",
+			entity.User{
+				ID:        "909b42f8-cce9-4d02-bce4-e7c7e28df550",
+				AuthToken: "bb68df68-964e-4f27-a225-0cbafdd6ce9f",
+				Name:      "9",
+				HighScore: 70,
+				Coin:      0,
+			},
+			nil,
+		},
 	}
-	mockService := new(userServiceFaker)
-	serviceF := service.NewUserService(mockService.UserRepository)
-	t.Logf("service %s", b.ID)
+	fservice := gomock.NewController(t)
+	m := NewMockUserServiceInterface(fservice)
 
-	// for _, tt := range table {
-	// 	t.Run(tt.testName, func(t *testing.T) {
-	// 		b, _ := serviceF.SelectUserByPrimaryKey(tt.id)
-	// 		if b == nil {
-	// 			return
-	// 		}
-	// 		// assert.Equal(t, tt.err, err)
-	// 		// assert.Equal(t, tt.user, b)
-	// 	})
-	// }
+	for _, tt := range table {
+		m.EXPECT().SelectUserByPrimaryKey(tt.id).Return(&tt.user, nil)
+		user, err := m.SelectUserByPrimaryKey(tt.id)
+		assert.Equal(t, user, &tt.user)
+		assert.Equal(t, nil, err)
+	}
+}
+
+func TestSelectUserByAuthTokenService(t *testing.T) {
+	table := []struct {
+		testName  string
+		authToken string
+		user      entity.User
+		err       error
+	}{
+		{
+			"FIRST TEST CASE: TestSelectUserByPrimaryKeyService",
+			"b187b9e0-08e6-42dd-a9b3-a900b137983c",
+			entity.User{
+				ID:        "78164dcf-6b7c-45e4-862a-2a0f6735a449",
+				AuthToken: "b187b9e0-08e6-42dd-a9b3-a900b137983c",
+				Name:      "whatt",
+				HighScore: 100,
+				Coin:      10000,
+			},
+			nil,
+		},
+		{
+			"SECOND TEST CASE: TestSelectUserByPrimaryKeyService",
+			"4ed9e2e7-cbba-4ab9-a669-14b688dfb245",
+			entity.User{
+				ID:        "829bdb53-f322-40bb-9327-63ab00536cd3",
+				AuthToken: "4ed9e2e7-cbba-4ab9-a669-14b688dfb245",
+				Name:      "bruh",
+				HighScore: 0,
+				Coin:      0,
+			},
+			nil,
+		},
+		{
+			"THIRD TEST CASE: TestSelectUserByPrimaryKeyService",
+			"bb68df68-964e-4f27-a225-0cbafdd6ce9f",
+			entity.User{
+				ID:        "909b42f8-cce9-4d02-bce4-e7c7e28df550",
+				AuthToken: "bb68df68-964e-4f27-a225-0cbafdd6ce9f",
+				Name:      "9",
+				HighScore: 70,
+				Coin:      0,
+			},
+			nil,
+		},
+	}
+	fservice := gomock.NewController(t)
+	m := NewMockUserServiceInterface(fservice)
+
+	for _, tt := range table {
+		m.EXPECT().SelectUserByAuthToken(tt.authToken).Return(&tt.user, nil)
+		user, err := m.SelectUserByAuthToken(tt.authToken)
+		assert.Equal(t, user, &tt.user)
+		assert.Equal(t, nil, err)
+	}
+}
+
+func TestInsertUserService(t *testing.T) {
+	table := []struct {
+		testName  string
+		authToken string
+		user      entity.User
+		err       error
+	}{
+		{
+			"FIRST TEST CASE: TestSelectUserByPrimaryKeyService",
+			"b187b9e0-08e6-42dd-a9b3-a900b137983c",
+			entity.User{
+				ID:        "78164dcf-6b7c-45e4-862a-2a0f6735a449",
+				AuthToken: "b187b9e0-08e6-42dd-a9b3-a900b137983c",
+				Name:      "whatt",
+				HighScore: 100,
+				Coin:      10000,
+			},
+			nil,
+		},
+		{
+			"SECOND TEST CASE: TestSelectUserByPrimaryKeyService",
+			"4ed9e2e7-cbba-4ab9-a669-14b688dfb245",
+			entity.User{
+				ID:        "829bdb53-f322-40bb-9327-63ab00536cd3",
+				AuthToken: "4ed9e2e7-cbba-4ab9-a669-14b688dfb245",
+				Name:      "bruh",
+				HighScore: 0,
+				Coin:      0,
+			},
+			nil,
+		},
+		{
+			"THIRD TEST CASE: TestSelectUserByPrimaryKeyService",
+			"bb68df68-964e-4f27-a225-0cbafdd6ce9f",
+			entity.User{
+				ID:        "909b42f8-cce9-4d02-bce4-e7c7e28df550",
+				AuthToken: "bb68df68-964e-4f27-a225-0cbafdd6ce9f",
+				Name:      "9",
+				HighScore: 70,
+				Coin:      0,
+			},
+			nil,
+		},
+	}
+	fservice := gomock.NewController(t)
+	m := NewMockUserServiceInterface(fservice)
+
+	for _, tt := range table {
+		m.EXPECT().InsertUser(&tt.user).Return(nil)
+		err := m.InsertUser(&tt.user)
+		assert.Equal(t, nil, err)
+	}
+}
+
+func TestUpdateUserByPrimaryKey(t *testing.T) {
+	table := []struct {
+		testName  string
+		authToken string
+		user      entity.User
+		err       error
+	}{
+		{
+			"FIRST TEST CASE: TestSelectUserByPrimaryKeyService",
+			"b187b9e0-08e6-42dd-a9b3-a900b137983c",
+			entity.User{
+				ID:        "78164dcf-6b7c-45e4-862a-2a0f6735a449",
+				AuthToken: "b187b9e0-08e6-42dd-a9b3-a900b137983c",
+				Name:      "whatt",
+				HighScore: 100,
+				Coin:      10000,
+			},
+			nil,
+		},
+		{
+			"SECOND TEST CASE: TestSelectUserByPrimaryKeyService",
+			"4ed9e2e7-cbba-4ab9-a669-14b688dfb245",
+			entity.User{
+				ID:        "829bdb53-f322-40bb-9327-63ab00536cd3",
+				AuthToken: "4ed9e2e7-cbba-4ab9-a669-14b688dfb245",
+				Name:      "bruh",
+				HighScore: 0,
+				Coin:      0,
+			},
+			nil,
+		},
+		{
+			"THIRD TEST CASE: TestSelectUserByPrimaryKeyService",
+			"bb68df68-964e-4f27-a225-0cbafdd6ce9f",
+			entity.User{
+				ID:        "909b42f8-cce9-4d02-bce4-e7c7e28df550",
+				AuthToken: "bb68df68-964e-4f27-a225-0cbafdd6ce9f",
+				Name:      "9",
+				HighScore: 70,
+				Coin:      0,
+			},
+			nil,
+		},
+	}
+	fservice := gomock.NewController(t)
+	m := NewMockUserServiceInterface(fservice)
+
+	for _, tt := range table {
+		m.EXPECT().UpdateUserByPrimaryKey(&tt.user).Return(nil)
+		err := m.UpdateUserByPrimaryKey(&tt.user)
+		assert.Equal(t, nil, err)
+	}
 }
