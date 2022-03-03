51c41ae23c49f8a8f841626e3028c525e145d579
daikidev111 <dkub0001@student.monash.edu>
2022-03-03 12:05:59 +0900

    Made file selection and model generator functions for readability

--- cmd/generator/generator.go
+++ cmd/generator/generator.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"html/template"
 	"log"
 	"os"
@@ -50,31 +51,25 @@ const (
 )
 
 //  go run generator.go -model="User" file=""
+
+// WARNING: space after the , will be determined as an invalid command
 func main() {
 	f := flag.String("model", "", "This is to generate files based on the model name")
+	m := flag.String("file", "", "This is to determine which files to generate")
 	flag.Parse()
-	words := strings.Fields(*f)
 
-	var model string
-	var modelFCharLowerCase string
-	if len(words) > 1 {
-		for _, v := range words {
-			model += strings.Title(strings.ToLower(v))
-		}
-		modelFCharLowerCase = FirstCharToLowerCase(model)
-	} else if len(words) == 1 {
-		model = strings.Title(strings.ToLower(*f))
-		modelFCharLowerCase = FirstCharToLowerCase(model)
-	} else {
-		log.Println("Does not accept empty word")
+	file := strings.Split(*m, ",")
+
+	model, modelFCharLowerCase := convertModelInputToModel(f)
+	if model == "" && modelFCharLowerCase == "" {
 		return
 	}
-	createEntityFile(model)
-	createDomainRepositoryFile(model)
-	createServiceFile(model, modelFCharLowerCase)
-	createDatabaseRepositoryFile(model)
-	createInteractorFile(model, modelFCharLowerCase)
-	createControllerFile(model, modelFCharLowerCase)
+
+	// Obtain the file in array and conduct a for loop that loops through to check if it has the fixed file name that is pre-determined in this tool
+	err := fileSelection(model, modelFCharLowerCase, file)
+	if err != nil {
+		fmt.Println("[ERROR] fileSelection(model, modelFCharLowerCase, file) = ", err)
+	}
 }
 
 func createEntityFile(model string) {
@@ -270,4 +265,73 @@ func FirstCharToLowerCase(model string) string {
 	a[0] = unicode.ToLower(a[0])
 	s = string(a)
 	return s
+}
+
+func fileSelection(model, modelFCharLowerCase string, file []string) error {
+	for _, v := range file {
+		v = strings.ToLower(v) // make all the chars lower case in case of some capitalized chars
+		validate := true
+		switch validate {
+		case v == "":
+			return fmt.Errorf("does not accept empty file")
+
+		case v == "database repository" || v == "data repo" || v == "database repo":
+			createDatabaseRepositoryFile(model)
+			fmt.Printf("successfully generated a file %s\n", v)
+
+		case v == "entity":
+			createEntityFile(model)
+			fmt.Printf("successfully generated a file %s\n", v)
+
+		case v == "domain repository" || v == "domain repo":
+			createDomainRepositoryFile(model)
+			fmt.Printf("successfully generated a file %s\n", v)
+
+		case v == "domain service":
+			fmt.Printf("successfully generated a file %s\n", v)
+			createServiceFile(model, modelFCharLowerCase)
+
+		case v == "interactor" || v == "int":
+			createInteractorFile(model, modelFCharLowerCase)
+			fmt.Printf("successfully generated a file %s\n", v)
+
+		case v == "controller" || v == "cont":
+			createControllerFile(model, modelFCharLowerCase)
+			fmt.Printf("successfully generated a file %s\n", v)
+
+		case v == "all":
+			createEntityFile(model)
+			createDomainRepositoryFile(model)
+			createServiceFile(model, modelFCharLowerCase)
+			createDatabaseRepositoryFile(model)
+			createInteractorFile(model, modelFCharLowerCase)
+			createControllerFile(model, modelFCharLowerCase)
+			fmt.Printf("successfully generated a file %s\n", v)
+
+		default:
+			return fmt.Errorf("error with the file generation. Please check if it is entered according to its instruction")
+
+		}
+
+	}
+	return nil
+}
+
+func convertModelInputToModel(modelInput *string) (string, string) {
+	words := strings.Fields(*modelInput)
+	var model string
+	var modelFCharLowerCase string
+	if len(words) > 1 {
+		for _, v := range words {
+			model += strings.Title(strings.ToLower(v))
+		}
+		modelFCharLowerCase = FirstCharToLowerCase(model)
+	} else if len(words) == 1 {
+		model = strings.Title(strings.ToLower(*modelInput))
+		modelFCharLowerCase = FirstCharToLowerCase(model)
+	} else {
+		log.Println("Does not accept empty word")
+		return "", ""
+	}
+	return model, modelFCharLowerCase
 }
